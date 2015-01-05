af54a1774e3ea5523b2675594f210e54611b30b4
Ben@laptop <Benjamin.l.johnson672@gmail.com>
2015-01-05 13:47:16 -0500

    updated the models mass assingment

--- app/config/otherapp.php
+++ app/config/otherapp.php
@@ -5,10 +5,11 @@
 |--------------------------------------------------------------------------
 | In our setting there are two apps, think of it as a reader and writer
 |--------------------------------------------------------------------------
+| We must tell the writer where it can write its images too.
+|  Basically, we have to give the foucs_admin app the path to where it can 
+|  write images into the focus_app
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| It can be used like Config::get('otherapp.images_path')
 |
 */
 return array(
